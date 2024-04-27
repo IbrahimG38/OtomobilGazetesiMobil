6af3ff491b8991745763923c7f445058a28be570
IbrahimG38 <gokibo@hotmail.com>
2024-04-27 13:27:44 +0300

    üçüncü gönderim (Keşfet ekranı kaba haliyle oluşturuldu, arama çubuğu eklendi.)

--- otoban/src/screens/KesfetEkrani.js
+++ otoban/src/screens/KesfetEkrani.js
@@ -1,10 +1,82 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
+import React, { useEffect, useState,} from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useColorScheme } from "nativewind";
+import { StatusBar } from "expo-status-bar";
+import { useQuery } from "@tanstack/react-query";
+
+import { MagnifyingGlassIcon } from "react-native-heroicons/outline";
+import { fetchDiscoverNews } from "../../utils/NewsApi";
+import { useNavigation } from "@react-navigation/native";
 
 export default function KesfetEkrani() {
+  const { colorScheme, toggleColorScheme } = useColorScheme();
+  const [activeCategory, setActiveCategory] = useState("business");
+  const navigation = useNavigation();
+  const [withoutRemoved, setWithoutRemoved] = useState([]);
+
+  useEffect(() => {}, [activeCategory]);
+
+  const { data: discoverNew, isLoading: isDiscoverLoading } = useQuery({
+    queryKey: ["discoverNews", activeCategory],
+    queryFn: () => fetchDiscoverNews(activeCategory),
+  });
+
+  const handleChangeCategory = (category) => {
+    setActiveCategory(category);
+
+    const filteredArticles = discoverNew?.articles.filter(
+      (article) => article.title !== "[Removed]"
+    );
+
+    setWithoutRemoved(filteredArticles || []);
+  };
+
   return (
-    <View>
-      <Text>KesfetEkrani</Text>
-    </View>
-  )
+    <SafeAreaView className="pt-8 bg-white dark:bg-neutral-900">
+      <StatusBar style={colorScheme == "dark" ? "light" : "dark"} />
+
+      <View>
+        {/* Headerım */}
+        <View className="px-4 mb-6 justify-between">
+          <Text
+            className="text-3xl text-green-800 dark:text-white"
+            style={{
+              fontFamily: "SpaceGroteskBold",
+            }}
+          >
+            Discover
+          </Text>
+
+          <Text
+            className="text-base text-gray-600 dark:text-neutral-300 "
+            style={{
+              fontFamily: "SpaceGroteskMedium",
+            }}
+          >
+            News from all over the world
+          </Text>
+        </View>
+
+         {/* Arama Çubuğu */}
+         <View className="mx-4 mb-8 flex-row p-2 py-3 justify-between items-center bg-neutral-100 rounded-full">
+          <TouchableOpacity className="pl-2">
+            <MagnifyingGlassIcon size="25" color="gray" />
+          </TouchableOpacity>
+          <TextInput
+            onPressIn={() => navigation.navigate("Arama")}
+            placeholder="Search for news"
+            placeholderTextColor={"gray"}
+            className="pl-4 flex-1 font-medium text-black tracking-wider"
+          />
+        </View>
+
+            </View>
+        </SafeAreaView>
+  );
 }
